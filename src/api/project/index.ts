d7d2cf1e4991c6e25270fa163f17d1b48b36a48f
Sasha <sashaandreev7@gmail.com>
2017-06-29 00:29:00 +0300

    add swagger api documentation

---
+++ src/api/project/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by sasha on 6/22/17.
+ */
+"use strict";
+
+import {Router} from "express";
+import {hasRole, isAuthenticated} from "../../auth/auth.service";
+import {controller} from "./project.controller";
+import {projectUpload} from "../../config/multer.config"
+// import {validateReauest} from "../../common/validation.service";
+
+export default function(): Router {
+    const router =  Router();
+    router.get("/", isAuthenticated(), controller.show);
+    router.delete("/:id", hasRole("admin"), controller.destroy);
+    router.put("/:id", isAuthenticated(), controller.update);
+    router.post("/",isAuthenticated(), projectUpload, controller.create);
+    return router;
+}
