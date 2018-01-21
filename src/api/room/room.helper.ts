075dbd5dfea068ab867964bc5f793403b2ab1e70
Sasha <sashaandreev7@gmail.com>
2018-01-21 17:26:27 +0200

    fix rooms

--- src/api/room/room.helper.ts
+++ src/api/room/room.helper.ts
@@ -2,6 +2,7 @@ import {ScocketServiceInstance} from "../../common/socket.service";
 
 export function checkUsersOnline() {
     return (room) => {
+        if (!room) return null;
         const onlineUsers = ScocketServiceInstance.room.getConnectedUsersId(room._id);
         room.users.forEach((user) => {
             user.UserToRoom.online = onlineUsers.has(user._id);
